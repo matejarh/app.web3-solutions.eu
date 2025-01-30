a14d22b0105388f21af6c1475a39a800021239ea
Matej Arh <matej.arh@gmail.com>
2025-01-31 00:48:38 +0100

    contact section + logo z napisi

---
+++ resources/js/Pages/Contact.vue
@@ -0,0 +1,36 @@
+<script setup>
+import CleanLayout from "@/Layouts/CleanLayout.vue";
+import TopNavigation from "@/Components/Landing/TopNavigation.vue";
+import { ref } from "vue";
+import PageFooter from "../Components/Landing/PageFooter.vue";
+import HeroSection from "@/Components/Contact/HeroSection.vue";
+import ContactInformationSection from "@/Components/Landing/ContactInformationSection.vue";
+
+
+defineProps({
+  canLogin: {
+    type: Boolean,
+  },
+  canRegister: {
+    type: Boolean,
+  },
+});
+const scrollTop = ref(0);
+
+const handleScroll = (n) => {
+  scrollTop.value = n;
+};
+</script>
+
+<template>
+  <CleanLayout title="Kontakt" vertical-aligment="top" @scroll-top="handleScroll">
+    <TopNavigation :scroll-top="scrollTop" />
+
+    <HeroSection id="home" />
+    <!-- <OurServices id="services" />
+    <WhyUs id="whyus" /> -->
+    <ContactInformationSection id="contact" />
+    <!-- <Epilog id="epilog" /> -->
+    <PageFooter id="footer" />
+  </CleanLayout>
+</template>
