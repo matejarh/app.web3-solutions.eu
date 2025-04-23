71b351f20a7c4ca81fda929a9f7f5ccad76453d1
Matej Arh <matej.arh@gmail.com>
2025-04-23 11:59:58 +0200

    fix: improve locale retrieval logic in SetLocale middleware

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -16,7 +16,14 @@ class SetLocale
      */
     public function handle($request, Closure $next)
     {
-        $locale = Session::get('locale', config('app.locale'));
+        $locale = Session::get('locale');
+
+        if (!$locale) {
+            $browserLocale = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
+            $locale = in_array($browserLocale, ['en', 'sl']) ? $browserLocale : config('app.locale');
+            Session::put('locale', $locale);
+        }
+
         App::setLocale($locale);
 
         return $next($request);
