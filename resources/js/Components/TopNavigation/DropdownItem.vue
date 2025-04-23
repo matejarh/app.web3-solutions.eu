bdd2109b5c25bb1dc241dcd54382602f50112435
Matej Arh <matej.arh@gmail.com>
2025-04-23 11:38:15 +0200

    fix: format code for consistency in DropdownItem component

--- resources/js/Components/TopNavigation/DropdownItem.vue
+++ resources/js/Components/TopNavigation/DropdownItem.vue
@@ -1,6 +1,6 @@
 <script setup>
 defineProps({
-    href:String,
+    href: String,
     as: {
         default: 'link',
         type: String,
@@ -16,11 +16,15 @@ defineEmits(['clicked'])
 </script>
 <template>
     <li>
-        <inertia-link v-if="as === 'link'" :href="href"
-            class="block py-2 px-4 text-sm hover:bg-primary-100 dark:hover:bg-primary-600 dark:text-primary-400 dark:hover:text-white"><slot /></inertia-link>
+        <inertia-link v-if="(as === 'link')" :href="href"
+            class="block py-2 px-4 text-sm hover:bg-primary-100 dark:hover:bg-primary-600 dark:text-primary-400 dark:hover:text-white">
+            <slot />
+        </inertia-link>
 
-            <button :type="type" @click="$emit('clicked')" class="block w-full py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white" v-if="as === 'button' || as === 'submit'" >
-                <slot />
-            </button>
+        <button :type="type" @click="$emit('clicked')"
+            class="block w-full py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+            v-if="(as === 'button' || as === 'submit')">
+            <slot />
+        </button>
     </li>
 </template>
